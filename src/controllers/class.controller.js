435f72391055bac71d4ecdaf785c7affe1431255
Kanishk <workforkanishk@gmail.com>
2026-02-26 19:24:25 +0530

    Class controller created

---
+++ src/controllers/class.controller.js
@@ -0,0 +1,73 @@
+const Class= require("../models/Class")
+const User= require("../models/User")
+
+const createClass= async (req,res)=>{
+    try{
+        const {className}= req.body;
+        const newClass= await Class.create({
+            className,
+            teacherId: req.user.userId,
+            studentIds: [],
+        });
+
+        return res.status(201).json({
+            success: true,
+            data: newClass,
+        });
+    }catch(error){
+        console.error(error);
+        return res.status(500).json({
+            success: false,
+            error: "Server error"
+        })
+    }
+}
+
+const addStudent= async (req,res)=>{
+    try{
+        const {id}= req.params;
+        const {studentId}= req.body;
+
+        const foundClass= await Class.findById(id);
+        if(!foundClass){
+            return res.status(404).json({
+                success: false,
+                error: "Class not found"
+            });
+        }
+        if(foundClass.teacherId.toString() !== req.user.userId){
+            return res.status(403).json({
+                success: false,
+                error: "Request denied, you are not the class teacher"
+            });
+        }
+
+        const student= await User.findById(studentId);
+        if(!student || student.role !== "student"){
+            return res.status(404).json({
+                success: false,
+                error: "Student doesn't exist"
+            })
+        }
+
+        if(foundClass.studentIds.includes(studentId)){
+            return res.status(400).json({
+                success: false,
+                error: "Student already enrolled in the class"
+            })
+        }
+        foundClass.studentIds.push(studentId);
+        await foundClass.save();
+        return res.status(200).json({
+            success: true,
+            data: foundClass,
+        });
+    } catch(error){
+        console.error(error);
+        return res.status(500).json({
+            success: false,
+            error: "Server error"
+        })
+    }
+
+}
